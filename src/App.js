93d053fbf80e23711628d6ec37bab7f3f10f52a5
Pranav Zende <pdzende99@gmail.com>
2024-06-19 10:51:04 +0530

    Create New Repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import HomePage from "./pages/HomePage";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HelmetProvider>
+      <BrowserRouter forceRefresh={true}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
---
+++ src/assets/imagePath.js
@@ -0,0 +1,2 @@
+export { default as logo } from "../assets/images/logo.png";
+export { default as fooLeft } from "../assets/images/foo-left.png";
